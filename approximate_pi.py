fa16ec1430d09774d9d4780ee488ab004de17718
Aurélien VILMINOT <aurelien.vilminot@etu.univ-amu.fr>
2020-10-13 22:35:36 +0200

    PPM header

--- approximate_pi.py
+++ approximate_pi.py
@@ -1,2 +1,51 @@
 #!/usr/bin/env python3
+"""
+Generation of PGM pictures as a GIF
+"""
+import sys
 import simulator
+
+PPM_FIRST_LINE = 'P3'
+PPM_THIRD_LINE = '255'
+
+def generate_ppm_file(image_size):
+    """
+    Generate picture in PPM format
+    """
+    ppm_second_line = str(image_size) + ' ' + str(image_size)
+    ppm_header = PPM_FIRST_LINE + '\n' + ppm_second_line + '\n' + PPM_THIRD_LINE + '\n'
+    print(ppm_header)
+
+
+def check_params(image_size, points_number, decimal_number):
+    """
+    Throw an exception if params are not int
+    """
+    try:
+        # Convert params into int
+        image_size = int(image_size)
+        points_number = int(points_number)
+        decimal_number = int(decimal_number)
+    except TypeError:
+        print("All params must be int type")
+    else:
+        return image_size, points_number, decimal_number
+
+def main():
+    """
+    Print the pi simulation if the program is called by command line
+    """
+    if len(sys.argv) != 4 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        print("Usage:", sys.argv[0], "image_size (int), points_number (int), decimal_number (int)")
+        sys.exit(1)
+
+    params = check_params(sys.argv[1], sys.argv[2], sys.argv[3])
+    image_size = params[0]
+    points_number = params[1]
+    decimal_number = params[2]
+
+    generate_ppm_file(image_size)
+
+
+if __name__ == "__main__":
+    main()
--- simulator.py
+++ simulator.py
@@ -6,7 +6,7 @@ import sys
 from random import uniform
 from collections import namedtuple
 
-# Define Point structure composed of attributes : x and y
+# Define Point structure composed of two attributes : x and y
 Point = namedtuple('Point', 'x y')
 
 CENTER_COORD = Point(0, 0)
@@ -18,19 +18,25 @@ def pi_simulation(nb_points):
 
     Params : nb_points is a int
     """
-    nb_points = int(nb_points)
-
     if not check_param(nb_points):
         sys.exit("An error has occurred")
 
     counter = 0
+    points_in_circle = []
+    points_out_circle = []
     for _ in range(1, nb_points + 1):
         x_coord = uniform(-1, 1)
         y_coord = uniform(-1, 1)
-        if is_in_circle(CENTER_COORD, RADIUS_CIRCLE, Point(x_coord, y_coord)):
+        point = Point(x_coord, y_coord)
+        if is_in_circle(CENTER_COORD, RADIUS_CIRCLE, point):
             counter += 1
+            points_in_circle.append(point)
+        else:
+            points_out_circle.append(point)
 
-    return 4 * (counter / nb_points)
+    pi_number = 4 * (counter / nb_points)
+
+    return pi_number, points_in_circle, points_out_circle
 
 def check_param(nb_points):
     """
@@ -49,7 +55,7 @@ def is_in_circle(center, radius, point):
     """
     Return true if the point is in the circle
 
-    Params : circle and point are Point
+    Params : circle and point are Point, radius is an int
     """
     return (point.x - center.x)**2 + (point.y - center.y)**2 <= radius**2
 
@@ -58,12 +64,11 @@ def main():
     Print the pi simulation if the program is called by command line
     """
     if len(sys.argv) != 2 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
-        print("Usage:", sys.argv[0], "number_of_points (int)")
+        print("Usage:", sys.argv[0], "points_number (int)")
         sys.exit(1)
 
-    nb_points = sys.argv[1]
-    print(pi_simulation(nb_points))
-
+    nb_points = int(sys.argv[1])
+    print(pi_simulation(nb_points)[0])
 
 if __name__ == "__main__":
     main()
